2b3e9cab870bb0cb1bf44c9b1c084458892cc94c
NicMortelliti <npmortelliti@gmail.com>
2022-08-22 15:38:17 -0700

    Add not found rescue

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,2 +1,14 @@
 class GymsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
+  def show
+    gym = Gym.find(params[:id])
+    render json: gym, status: :ok
+  end
+
+  private
+
+  def render_record_not_found
+    render json: { error: 'Gym not found' }, status: :not_found
+  end
 end
