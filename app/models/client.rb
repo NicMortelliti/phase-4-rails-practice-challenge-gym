4afddcc5dc1ac5f52f422dd261c10cab54f98693
NicMortelliti <npmortelliti@gmail.com>
2022-08-22 15:18:49 -0700

    Add recovery and create membership route

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ApplicationRecord
+  validates :gym_id, uniqueness: true
+
   has_many :memberships
   has_many :gyms, through: :memberships
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,8 @@
 class Membership < ApplicationRecord
+  validates :gym_id, null: false
+  validates :client_id, null: false
+  validates :charge, null: false
+
   belongs_to :client
   belongs_to :gym
 end
